f6673ee915de34c6db589875568e3e26a0ce6c61
JMCisnerosP <sistemrespaldos@gmail.com>
2026-04-13 21:43:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,13 @@ def enviar():
                 f.read()
             )
 
-    print("🚀 Enviando correo...")
-    mail.send(msg)
-    print("✅ Correo enviado")
+       try:
+        print("🚀 Enviando correo...")
+        mail.send(msg)
+        print("✅ Correo enviado")
+    except Exception as e:
+        print("❌ Error al enviar correo:", str(e))
+        return f"Error al enviar correo: {str(e)}"
    
     return render_template('confirmacion.html', datos=datos, monto=datos['monto'])
 
